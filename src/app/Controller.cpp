4e678decdd4b8e8f48d47e14085950f80e749dcf
Long Nguyen <long.nguyen2306@protonmail.com>
2021-03-12 18:07:00 +0100

    Reduced the number of points by factor 2

--- src/app/Controller.cpp
+++ src/app/Controller.cpp
@@ -9,55 +9,55 @@ namespace fft::app {
         // Load sound file into buffer
 
         fft::utils::TimeIt loading_sound("Loading sound from buffer");
-        if (!sound_buffer.loadFromFile("../../data/dido.wav")) {
+        if (!this->sound_buffer.loadFromFile("../../data/dido.wav")) {
             std::cerr << "Can not load file elise.wav. Please place the file in the correct path." << std::endl;
             std::exit(1);
         }
 
-        std::cout << sound_buffer << std::endl;
+        std::cout << this->sound_buffer << std::endl;
         //std::exit(0);
 
-        sound.setBuffer(sound_buffer);
+        this->sound.setBuffer(this->sound_buffer);
         loading_sound.end();
 
-        channel_size = sound_buffer.getSampleCount() / sound_buffer.getChannelCount();
+        this->channel_size = this->sound_buffer.getSampleCount() / this->sound_buffer.getChannelCount();
 
         // Centering the sound belong axis channels
         //
         // Resulting into 1D data, which we can comfortablely transform into complex numbers
         fft::utils::TimeIt centering_data("Centering data");
-        auto mean = fft::core::mean_channel(sound_buffer);
+        auto mean = fft::core::mean_channel(this->sound_buffer);
         centering_data.end();
 
         // Transform sample data into complex number
         fft::utils::TimeIt making_complex("Making data complex");
-        mean_data = std::make_unique<fft::core::Complex[]>(channel_size);
+        this->mean_data = std::make_unique<fft::core::Complex[]>(this->channel_size);
 
 #pragma omp parallel for
-        for (uint64_t i = 0; i < channel_size; i++) {
-            mean_data[i] = fft::core::Complex(mean[i], 0.0);
+        for (uint64_t i = 0; i < this->channel_size; i++) {
+            this->mean_data[i] = fft::core::Complex(mean[i], 0.0);
         }
         making_complex.end();
 
         // Making the sound loop
-        sound.setLoop(true);
+        this->sound.setLoop(true);
     }
 
     void Controller::start() {
         // Start to play sound
-        sound.play();
+        this->sound.play();
 
-        while (gui->isOpen()) {
-            while (gui->pollEvent(event)) {
-                switch (event.type) {
+        while (this->gui->isOpen()) {
+            while (this->gui->pollEvent(event)) {
+                switch (this->event.type) {
                     case sf::Event::Closed:
-                        gui->close();
+                        this->gui->close();
                         break;
                     default:
                         break;
                 }
             }
-            gui->clear();
+            this->gui->clear();
 
             /*
              * We want to pick the played segment in the last time delta
@@ -67,14 +67,14 @@ namespace fft::app {
              * So we have to do all the calculation live
              */
             // Obtaining the current sound offset
-            double now = sound.getPlayingOffset().asSeconds();
+            double now = this->sound.getPlayingOffset().asSeconds();
 
             // Obtaining the start and the stop of the played segment
-            uint64_t stop = now * sound_buffer.getSampleRate();
-            if(stop < segment_size) {
+            uint64_t stop = now * this->sound_buffer.getSampleRate();
+            if(stop < this->segment_size) {
                 continue;
             }
-            uint64_t start = stop - segment_size;
+            uint64_t start = stop - this->segment_size;
 
             // Creating buffer for the played segment
             //
@@ -83,11 +83,11 @@ namespace fft::app {
             // Since FFT only works for sample size of power 2^N, we need to do some padding
             // if the current data is not ideal
             fft::utils::TimeIt copying_segment("Copying playing segment");
-            auto played_segment = std::make_unique<fft::core::Complex[]>(segment_size);
-            auto played_segment_real = std::make_unique<int16_t []>(segment_size);
-            for(uint64_t i = 0; i < segment_size; i++) {
-                played_segment[i] = mean_data[start + i];
-                played_segment_real[i] = mean_data[start + i].real();
+            auto played_segment = std::make_unique<fft::core::Complex[]>(this->segment_size);
+            auto played_segment_real = std::make_unique<int16_t []>(this->segment_size);
+            for(uint64_t i = 0; i < this->segment_size; i++) {
+                played_segment[i] = this->mean_data[start + i];
+                played_segment_real[i] = this->mean_data[start + i].real();
             }
             copying_segment.end();
 
@@ -96,8 +96,8 @@ namespace fft::app {
             // OMP threading seem to create an overhead which is not worth it
             // Therefore we won't parallelize this loop
             fft::utils::TimeIt hanning_smoothing("Hanning smoothing");
-            auto hanning_coefficients = fft::core::hanning(segment_size);
-            for(uint64_t i = 0; i < segment_size; i++) {
+            auto hanning_coefficients = fft::core::hanning(this->segment_size);
+            for(uint64_t i = 0; i < this->segment_size; i++) {
                 played_segment[i] *= hanning_coefficients[i];
                 played_segment_real[i] *= hanning_coefficients[i].real();
             }
@@ -105,18 +105,18 @@ namespace fft::app {
 
             // Run FFT and power spectrum
             fft::utils::TimeIt fft("FFT transformation");
-            fft::core::fft(played_segment.get(), segment_size);
-            auto power_spectrum = fft::core::power_spectrum(played_segment.get(), segment_size);
+            fft::core::fft(played_segment.get(), this->segment_size);
+            auto power_spectrum = fft::core::power_spectrum(played_segment.get(), this->segment_size);
             fft.end();
 
             fft::utils::TimeIt visualization("Visualization");
             // Time to visualize_frequency_domain the spectrum
-            this->gui->visualize_frequency_domain(power_spectrum.get(), segment_size);
-            this->gui->visualize_time_domain(played_segment_real.get(), segment_size);
+            this->gui->visualize_frequency_domain(power_spectrum.get(), this->segment_size);
+            this->gui->visualize_time_domain(played_segment_real.get(), this->segment_size);
             visualization.end();
 
             // Refresh mainPanel
-            gui->display();
+            this->gui->display();
         }
     }
 }
--- src/app/GUI.cpp
+++ src/app/GUI.cpp
@@ -14,22 +14,22 @@ fft::app::GUI::GUI(sf::RenderWindow *renderWindow, uint64_t segment_size) : rend
      * Pre-initialize the power bins so we do not have to that every loop
      */
     for (uint64_t i = 0; i < segment_size - 1; i++) {
-        power_bins[i].setFillColor(sf::Color::White);
-        power_bins[i].setPosition(log10((float)i) / log10(segment_size) * renderWindow->getSize().x,
+        this->power_bins[i].setFillColor(sf::Color::White);
+        this->power_bins[i].setPosition(std::log10((float)i) / std::log10(this->segment_size) * this->renderWindow->getSize().x,
                                   renderWindow->getSize().y);
 
-        power_bins[i + 1].setFillColor(sf::Color::Black);
-        power_bins[i + 1].setPosition(log10(i + 1) / log10(segment_size) * renderWindow->getSize().x,
+        this->power_bins[i + 1].setFillColor(sf::Color::Black);
+        this->power_bins[i + 1].setPosition(std::log10(i + 1) / std::log10(this->segment_size) * this->renderWindow->getSize().x,
                                       renderWindow->getSize().y);
 
-        power_bins[i].setSize({power_bins[i + 1].getPosition().x - power_bins[i].getPosition().x, 0});
+        this->power_bins[i].setSize({this->power_bins[i + 1].getPosition().x - this->power_bins[i].getPosition().x, 0});
     }
 
     /*
      * Pre initialize time domain line
      */
-    for(uint64_t i = 0; i < time_domain.getVertexCount(); i++) {
-        time_domain[i].color = sf::Color{225, 225, 255, 255};
+    for(uint64_t i = 0; i < this->time_domain.getVertexCount(); i++) {
+        this->time_domain[i].color = sf::Color{225, 225, 255, 255};
     }
 
     this->random_end_color();
@@ -37,11 +37,11 @@ fft::app::GUI::GUI(sf::RenderWindow *renderWindow, uint64_t segment_size) : rend
 }
 
 void fft::app::GUI::random_end_color() {
-    this->end_color = *select_randomly(cold_colors.begin(), cold_colors.end());
+    this->end_color = *select_randomly(this->cold_colors.begin(), this->cold_colors.end());
 }
 
 void fft::app::GUI::random_start_color() {
-    this->start_color = *select_randomly(hot_colors.begin(), hot_colors.end());
+    this->start_color = *select_randomly(this->hot_colors.begin(), this->hot_colors.end());
 }
 
 void fft::app::GUI::close() {
@@ -58,11 +58,11 @@ bool fft::app::GUI::pollEvent(sf::Event &event) {
 
 void fft::app::GUI::clear() {
     // Drawing white backgroundß
-    renderWindow->clear(sf::Color::Black);
+    this->renderWindow->clear(sf::Color::Black);
 }
 
 void fft::app::GUI::display() {
-    renderWindow->display();
+    this->renderWindow->display();
 }
 
 void fft::app::GUI::visualize_frequency_domain(const float *spectrum, uint64_t size) {
@@ -80,7 +80,7 @@ void fft::app::GUI::visualize_bars(const float *spectrum) {
      * For this purpose we need to find the most frequent frequency
      */
     float max = spectrum[0];
-    for (uint64_t i = 0; i < segment_size; i++) {
+    for (uint64_t i = 0; i < this->segment_size; i++) {
         if (spectrum[i] > max) {
             max = spectrum[i];
         }
@@ -89,11 +89,11 @@ void fft::app::GUI::visualize_bars(const float *spectrum) {
     /*
      * We do log scale on the X-Axis since human ears work on log scale (the same working principle of the unit decibel)
      */
-    for (uint64_t i = 0; i < segment_size; i++) {
-        float height = (spectrum[i] / max) * (renderWindow->getSize().y / 10.0f);
+    for (uint64_t i = 0; i < this->segment_size; i++) {
+        float height = (spectrum[i] / max) * (this->renderWindow->getSize().y / 10.0f);
 
-        power_bins[i].setPosition(power_bins[i].getPosition().x, renderWindow->getSize().y - height);
-        power_bins[i].setSize({power_bins[i].getSize().x, height});
+        this->power_bins[i].setPosition(this->power_bins[i].getPosition().x, this->renderWindow->getSize().y - height);
+        this->power_bins[i].setSize({this->power_bins[i].getSize().x, height});
 
         this->renderWindow->draw(power_bins[i]);
     }
@@ -102,10 +102,10 @@ void fft::app::GUI::visualize_bars(const float *spectrum) {
 void fft::app::GUI::visualize_history() {
     // Calculate the newest history's color
     double percent = (double)this->current_color / (double)this->history.getCap();
-    uint8_t resultRed = start_color.r + percent * (end_color.r - start_color.r);
-    uint8_t resultGreen = start_color.g + percent * (end_color.g - start_color.g);
-    uint8_t resultBlue = start_color.b + percent * (end_color.b - start_color.b);
-    sf::Color color{resultRed, resultGreen, resultBlue, end_color.a};
+    uint8_t resultRed = this->start_color.r + percent * (this->end_color.r - this->start_color.r);
+    uint8_t resultGreen = this->start_color.g + percent * (this->end_color.g - this->start_color.g);
+    uint8_t resultBlue = this->start_color.b + percent * (this->end_color.b - this->start_color.b);
+    sf::Color color{resultRed, resultGreen, resultBlue, this->end_color.a};
     if(this->increasing) {
         this->current_color++;
     }else {
@@ -120,30 +120,27 @@ void fft::app::GUI::visualize_history() {
     }
 
     // Creating newest history
-    sf::VertexArray line(sf::PrimitiveType::LineStrip, segment_size * 2 - 2);
+    sf::VertexArray line(sf::PrimitiveType::LineStrip, this->segment_size);
 #pragma omp parallel for
-    for (uint64_t i = 0; i < segment_size - 1; i++) {
-        line[i * 2].position.x = power_bins[i].getPosition().x;
-        line[i * 2].position.y = power_bins[i].getPosition().y;
-
-        line[i * 2 + 1].position.x = power_bins[i + 1].getPosition().x;
-        line[i * 2 + 1].position.y = power_bins[i].getPosition().y;
-
-        line[i * 2 + 1].color = color;
-        line[i * 2].color = color;
+    for (uint64_t i = 0; i < segment_size; i++) {
+        line[i].position.x = power_bins[i].getPosition().x;
+        line[i].position.y = power_bins[i].getPosition().y;
+        line[i].color = color;
     }
     this->history.push_back(line);
 
     // Drawing every history line
     sf::View original_view = this->renderWindow->getView();
+
     for(uint64_t i = 0; i < this->history.size(); i++) {
         sf::View history_view;
-        history_view.setCenter(this->renderWindow->getSize().x / 2, this->renderWindow->getSize().y / 2);
-        history_view.move({(float)((float)i * 0.4), (float)(i * 2.5)});
+        history_view.setCenter(original_view.getCenter());
+        history_view.move({0, (float)(i * 2.5)});
         this->renderWindow->setView(history_view);
         this->renderWindow->draw(this->history[this->history.size() - 1 - i]);
         this->renderWindow->setView(original_view);
     }
+
 }
 
 void fft::app::GUI::visualize_time_domain(const int16_t *spectrum, uint64_t size) {
@@ -155,8 +152,8 @@ void fft::app::GUI::visualize_time_domain(const int16_t *spectrum, uint64_t size
     }
 #pragma omp parallel for
     for(uint64_t i = 0; i < this->time_domain.getVertexCount() ; i++) {
-        time_domain[i].position.x = ((float)i / (float)this->time_domain.getVertexCount()) * this->renderWindow->getSize().x;
-        time_domain[i].position.y = 150 + (spectrum[i * 10] / max) * 50.0;
+        this->time_domain[i].position.x = ((float)i / (float)this->time_domain.getVertexCount()) * this->renderWindow->getSize().x;
+        this->time_domain[i].position.y = 150 + (spectrum[i * 10] / max) * 50.0;
     }
     this->renderWindow->draw(time_domain);
 }
